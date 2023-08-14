ad89f757932550173aaa0b97bebc9e7b5e69a286
Akshat Sharma <webdevakshat@gmail.com>
2023-08-14 12:00:31 +0530

    Wrote Code Comments.

--- js/dynamichtml.js
+++ js/dynamichtml.js
@@ -3,17 +3,21 @@ const header = document.querySelector(".header");
 const meanings = document.querySelector(".meanings");
 
 export const renderWordHeaderWithAudio = (word, phonetics) => {
+  // Destructuring The Text And Audio Into Seperate Variables.
   const { text, audio } = getPhoneticAndAudio(phonetics);
 
+  // Inserting The Variables. In The HTML Template..
   const headerContent = headerTemplate
     .replace("{word}", word)
     .replace("{text}", text);
 
+  // Displaying The HTml.
   header.innerHTML = headerContent;
 
   const pronunciation = new Audio(audio);
 
   const audioHtml = document.querySelector(".audio");
+  // Playing The Pronounciation When User Clicks The Audio Button.
   audioHtml.addEventListener("click", () => {
     pronunciation.play();
   });
@@ -23,6 +27,7 @@ const getPhoneticAndAudio = (phoneticsObj) => {
   let text;
   let audio;
 
+  // Mapping Through The Phenotics Object And Finding The Text And Audio and assigning them to variables.
   phoneticsObj.forEach((item) => {
     if (!text && item.text) {
       text = item.text;
@@ -36,6 +41,7 @@ const getPhoneticAndAudio = (phoneticsObj) => {
 };
 
 export const renderMeanings = (results) => {
+  // Mapping Through The Results Object And Inserting The HTML.
   results.forEach((item) => {
     meanings.insertAdjacentHTML(
       "beforeend",
--- js/index.js
+++ js/index.js
@@ -2,22 +2,28 @@ import { renderMeanings, renderWordHeaderWithAudio } from "./dynamichtml.js";
 const query = document.querySelector(".query");
 const searchButton = document.querySelector(".search-btn");
 
+// This Function Makes a call to the dictionary api and then get the corresponding data.
 const dictionaryLookup = async (query) => {
   const api = await fetch(
     `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`
   );
   const resp = await api.json();
+  // Data is in object form so the main data we want is on the first index so directly returning that data.
   return resp[0];
 };
 
 async function showData(e) {
+    // Stopping The Page Refresh On Submitting
   e.preventDefault();
+  // Making A Call To The Api.
   const data = await dictionaryLookup(query.value);
+  // If the query is invalid and there is no data related to that so user will be alerted and the function will stop procceding.
   if (!data) {
     alert("Result Not Found.");
     return;
   }
 
+  // Rendering The Html Code To Display Results.
   renderWordHeaderWithAudio(query.value, data.phonetics);
   renderMeanings(data.meanings);
 }
